907f1767573fce542570656652e8a039a554ab84
BuildTools <arthurpisakhov1@gmail.com>
2021-03-08 17:37:24 -0500

    dev deploy url fixed, netlify redirect, and backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: [process.env.FRONTEND_URL, "https://www.your-cookbook.us"]
+      origin: [process.env.FRONTEND_URL, "https://your-cookbook.netlify.app]
     }
   },
   () =>
@@ -18,7 +18,7 @@ server.start(
       `Server running at ${
         process.env.FRONTEND_URL
           ? "http://localhost:4000"
-          : "https://www.your-cookbook.us"
+          : "https://your-cookbook.netlify.app"
       }`
     )
 );
